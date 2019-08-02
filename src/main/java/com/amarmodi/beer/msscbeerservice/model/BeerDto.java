49abf716370b4c7cb22aff7970d4eefe33fb3b0a
Amar <you@example.com>
2019-08-01 21:02:32 -0400

    Adding the Beer and BeerList objs

---
+++ src/main/java/com/amarmodi/beer/msscbeerservice/model/BeerDto.java
@@ -0,0 +1,29 @@
+package com.amarmodi.beer.msscbeerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Slf4j
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/amarmodi/beer/msscbeerservice/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.amarmodi.beer.msscbeerservice.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
