76766eefa8176755638dae5be2f528ca5a8f9ff6
Amar <you@example.com>
2019-08-05 19:57:29 -0400

    Adding Beer repository

---
+++ src/main/java/com/amarmodi/beer/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.amarmodi.beer.msscbeerservice.repositories;
+
+import com.amarmodi.beer.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Long, Beer> {
+
+}
