5d2ff4a5faef0e26ceafcd532ab27ebed5bb52a1
Amar <you@example.com>
2019-08-05 18:11:57 -0400

    adding change to unit tesgt

--- src/test/java/com/amarmodi/beer/msscbeerservice/controller/BeerControllerTest.java
+++ src/test/java/com/amarmodi/beer/msscbeerservice/controller/BeerControllerTest.java
@@ -2,7 +2,6 @@ package com.amarmodi.beer.msscbeerservice.controller;
 
 import com.amarmodi.beer.msscbeerservice.model.BeerDto;
 import com.amarmodi.beer.msscbeerservice.model.BeerStyleEnum;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
