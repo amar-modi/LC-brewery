3351a95fc91aa90db2a608a2afe86762bdf5dfe4
Amar <you@example.com>
2019-08-05 21:11:44 -0400

    adding the controller advice

--- src/main/java/com/amarmodi/beer/msscbeerservice/controller/BeerController.java
+++ src/main/java/com/amarmodi/beer/msscbeerservice/controller/BeerController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -20,12 +21,12 @@ public class BeerController {
     }
 
     @PostMapping("")
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto){
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
